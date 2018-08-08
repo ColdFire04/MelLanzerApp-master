981d1938dc01eecf41e61956b8d5d8278b41d660
ColdFire04 <calengray@icloud.com>
2018-08-07 21:31:30 -0400

    added features

--- app/src/main/java/com/example/calengray/mellanzer/newJobFragment.java
+++ app/src/main/java/com/example/calengray/mellanzer/newJobFragment.java
@@ -9,40 +9,17 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
-public class newJobFragment extends Fragment implements View.OnClickListener{
-
-    private EditText job_input;
-    private Button goButton;
-    private Button display_button;
-    String text;
+public class newJobFragment extends Fragment{
 
     public newJobFragment() {
-        // Required empty public constructor
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_new_job, container, false);
+        return inflater.inflate(R.layout.fragment_new_job, container, false);
 
-
-
-        display_button = (Button) view.findViewById(R.id.display_button);
-
-        goButton.setOnClickListener(this);
-
-            return view;
     }
 
-    public void onClick(View view) {
-        if(view.getId() == goButton.getId());
-        text=job_input.getText().toString();
-        display_button.setText(text);
-        }
 }
